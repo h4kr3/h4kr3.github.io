6c38193ff41edede7e3d7f9ddabf8e47facbf94d
Mrh4r1 <hk8031@icloud.com>
2022-10-29 18:03:22 +0530

    update.0

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var nameError=document.getElementById('name-error');
 var phoneError=document.getElementById('phone-error');
 var emailError=document.getElementById('email-error');
 var messageError=document.getElementById('message-error');
+var submitError=document.getElementById('submit-error');
 
 function validateName() {
 var name=document.getElementById('contact-name').value;
@@ -14,11 +15,16 @@ if(name.length==0) {
     nameError.innerHTML="Name is required";
     return false;
 }
-if(!name.match(/^[A-Za-z]*\{1}[A-Za-z]*&/)) {
+if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
     nameError.innerHTML="write full name";
     return false;
 
 }
+nameError.innerHTML= '<i class="bi bi-check-all"></i>';
+return true;
+}
+
+function validatePhone() {
 }
 
 
@@ -39,32 +45,54 @@ function validatePhone() {
         return false;
     
     }
+    phoneError.innerHTML= '<i class="bi bi-check-all"></i>';
+    return true;
 
 
     }
     function validateEmail() {
-        var email=document.getElementById('contact-phone').value;
+        var email=document.getElementById('contact-email').value;
         
         if(email.length==0) {
             emailError.innerHTML="Email is required";
             return false;
         }
-        if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.]{2;4}$/)) {
+        if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) 
+        {
             emailError.innerHTML="Email invalid";
             return false;
         
         }
+        emailError.innerHTML='<i class="bi bi-check-all"></i>';
+        return true;
     
         }
 
         function validateMessage() {
-            var message=document.getElementById('contact-phone').value;
-            
+            var message=document.getElementById('contact-message').value;
+            var required=30;
+            var left=required-message.length;
+            if(left>0) {
+                messageError.innerHTML="Message is too short";
+                return false;
+            }
+
             if(message.length==0) {
                 messageError.innerHTML="Message is required";
                 return false;
             }
+            messageError.innerHTML='<i class="bi bi-check-all"></i>';
+            return true;
         
             }
     
+        function validateForm() {
+            if(!validateName() || !validatePhone()||!validateEmail()||!validateMessage()){
+                submitError.style.display="block";
+                submitError.innerHTML='please fill the form to';
+                setTimeout(function(){submitError.style.display="none";},3000);
+                return false;
+            }
+
+        }
 
